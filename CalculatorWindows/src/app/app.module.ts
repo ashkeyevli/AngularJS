81b6269aab405a86f9f09e711c4f0e7d2f9de7a6
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-09-24 21:26:25 +0600

    more func

--- CalculatorWindows/src/app/app.module.ts
+++ CalculatorWindows/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { A2Component } from './a2/a2.component';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    A2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CalculatorWindows/src/app/calculator/calculator.component.ts
+++ CalculatorWindows/src/app/calculator/calculator.component.ts
@@ -6,56 +6,93 @@ import {formatNumber} from '@angular/common';
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent implements OnInit{
+export class CalculatorComponent {
   result = '';
-
-  buttons: string[] = ['%', 'CE', 'C', 'b', '1/x', 'x^2', 'sqrt', '/', '7', '8', '9', '*', '4', '5', '6'
-    , '-', '1', '2', '3', '+', '+/-', '0', '.', '='];
+  line1 = '';
+  public history1: string [] = [];
+  numbers: string[] = ['7', '8', '9', '4', '5', '6', '1', '2', '3', ];
+  lowLine: string[] = [ '+/-', '0', '.'];
+  highLine1: string[] = ['%', 'CE', 'C'];
+  highLine2: string[] = ['1/x', 'x^2', 'sqrt'];
+  operations: string[] = ['Backspace', '/', '*', '-', '+', '='];
+  memoryButtons: string[] = ['MC', 'MR', 'M+', 'M-', 'MS', 'M'];
+  public memory: string[] = [];
   private preValue = '';
-  private answer = 0;
+  public showHistory = false;
   private curValue = '';
-  constructor() { }
-  ngOnInit(): void {}
 
 
-  addToExpression(value: string){
-    if (this.result !== ''){
-      this.preValue = this.curValue;
-      this.curValue = value;
-    }
 
 
-    if (value === 'C'){
-      this.result = '';
-    }else if (value === 'b') {
-      this.result = this.preValue !== '=' ? this.result.slice(0, -1) : this.result;
-    }
-    else if (value === '1/x'){
-      this.result = (1.0 / parseFloat(this.result)).toString();
-    }
-    else if (value === 'x^2'){
-      this.result = (Math.pow(parseFloat(this.result), 2)).toString();
-    }
-    else if (value === 'sqrt'){
-      this.result = (Math.pow(parseFloat(this.result),  1 / 2)).toString();
-    }
-
-    else if (value === '+/-'){
-      console.log(this.curValue, this.preValue, this.result)
-      if (parseFloat(this.result) > 0){
-        this.result = '-' + this.result;
-      } else{
-        this.result = (this.result).toString().slice(1, this.result.length);
+  addToExpression(value: string) {
+    try {
+      if (this.result !== '') {
+        this.preValue = this.curValue;
+        this.curValue = value;
       }
-    }
-    else if (value === '='){
-      // tslint:disable-next-line:no-eval
-      this.result = eval(this.result);
 
-    }else{
-      this.result += value;
-    }
 
+      if (value === 'C') {
+        this.result = '';
+      } else if (value === 'Backspace') {
+        this.result = this.preValue !== '=' ? this.result.slice(0, -1) : this.result;
+      } else if (value === '1/x') {
+        this.result = (1.0 / parseFloat(this.result)).toString();
+        this.saveHistory(this.result);
+      }else if (value === '%') {
+        console.log(this.preValue, this.result, this.curValue);
+        this.result = ((parseFloat(this.result) / 100)).toString();
+        this.saveHistory(this.result);
+        console.log(this.preValue, this.line1, this.curValue);
+      } else if (value === 'x^2') {
+        this.result = (Math.pow(parseFloat(this.result), 2)).toString();
+        this.saveHistory(this.result);
+      }  else if (value === 'sqrt') {
+        this.result = (Math.pow(parseFloat(this.result), 1 / 2)).toString();
+        this.saveHistory(this.result);
+      } else if (value === '+/-') {
+        console.log(this.curValue, this.preValue, this.result);
+        if (parseFloat(this.result) > 0) {
+          this.result = '-' + this.result;
+        } else {
+          this.result = (this.result).toString().slice(1, this.result.length);
+        }
+      } else if (value === '=') {
+        // tslint:disable-next-line:no-eval
+        this.saveHistory(this.result + '=' + eval(this.result));
+        this.line1 = this.result + '=';
+        this.result = eval(this.result);
+
+
+
+        console.log(this.history1);
+
+      } else {
+        this.line1 = '';
+        this.result += value;
+      }
+
+
+    } catch (ExceptionInformation) {
+      this.result = '';
+      this.line1 = '';
+      this.result = 'Invalid Operation, Press C';
+    }
+  }
+
+  saveHistory(result: string){
+    this.history1.push(result);
+  }
+
+  history(){
+    if (this.showHistory){
+      return this.showHistory = false;
+    }
+    return this.showHistory = true;
+  }
+
+  clearHistory(){
+    this.history1 = [];
   }
 
 
